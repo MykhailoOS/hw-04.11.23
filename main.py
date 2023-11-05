0d2a11b2773a14f84d2bc5e1cc4bd17ec64bf9f2
MPO <bdemlolp234@gmail.com>
2023-11-05 17:04:42 +0200

    added first part task

--- main.py
+++ main.py
@@ -1,3 +1,99 @@
+# First part of homework
+# Task1
+class Discount:
+    def discount(self):
+        raise NotImplementedError
+
+
+class RegularDiscount(Discount):
+    def discount(self):
+        return 0.9
+
+
+class SilverDiscount(Discount):
+    def discount(self):
+        return 0.85
+
+
+class GoldDiscount(Discount):
+    def discount(self):
+        return 0.8
+
+
+class Product:
+    def __init__(self, title: str, price: float | int):
+        self.title = title
+        self.price = price
+
+    def __str__(self):
+        return f'{self.title}: {self.price:.2f}'
+
+
+
+class CartIterator:
+
+    def __init__(self, products, quantities):
+        self.products = products
+        self.quantities = quantities
+        self.index = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.index < len(self.products):
+            self.index += 1
+            return self.products[self.index - 1], self.quantities[self.index - 1]
+        raise StopIteration
+
+
+class Cart:
+    def __init__(self, discount: Discount = None):
+        self.__products = []
+        self.__quantities = []
+        self.discount = discount
+
+    def add_product(self, product: Product, quantity: int | float = 1):
+        if isinstance(product, Product) and isinstance(quantity, int | float):
+            self.__products.append(product)
+            self.__quantities.append(quantity)
+
+    def __iter__(self):
+        return CartIterator(self.__products, self.__quantities)
+
+    def total(self):
+        summa = sum(product.price * quantity for product, quantity in zip(self.__products, self.__quantities))
+        return summa * self.discount.discount() if self.discount else summa
+
+    def __iadd__(self, other):
+        return Cart()
+
+    def __str__(self):
+        return '\n'.join(map(lambda items: f'{items[0]} x {items[1]} = {items[0].price * items[1]} UAH',
+                             zip(self.__products, self.__quantities))) + f'\nTotal: {self.total():.2f} UAH\n'
+
+
+pr_1 = Product('banana', 50)
+pr_2 = Product('apple', 51)
+pr_3 = Product('orange', 52)
+
+cart_1 = Cart()
+cart_2 = Cart(SilverDiscount())
+cart_1 += cart_2
+cart_1.add_product(pr_1)
+cart_1.add_product(pr_2)
+cart_1.add_product(pr_3)
+
+cart_1.add_product(pr_1, 5)
+for i in cart_1:
+    print('\n'.join(map(str, i)))
+
+
+
+
+
+
+# Second part of homework
 # Task1
 def generator_mul(stop):
 
